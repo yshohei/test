aafae438567249884f2276d73ae3949358b67322
yshohei <hop.step.jump828@gmail.com>
2018-03-30 11:44:01 +0900

    javafile

---
+++ template/src/com/internousdev/template/dao/BuyItemDAO.java
@@ -0,0 +1,33 @@
+package com.internousdev.template.dao;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import com.internousdev.template.util.DBConnector;
+import com.internousdev.template.dto.BuyItemDTO;
+
+public class BuyItemDAO {
+	private DBConnector dbConnector = new DBConnector();
+	private Connection connection = dbConnector.getConnection();
+	private BuyItemDTO buyItemDTO = new BuyItemDTO();
+
+	public BuyItemDTO getBuyItemInfo(){
+		String sql = "SELECT id, item_name, item_price FROM item_info_transaction";
+
+		try{
+			PreparedStatement prepareStatement = connection.prepareStatement(sql);
+			ResultSet resultSet = prepareStatement.executeQuery();
+			if(resultSet.next()){
+				buyItemDTO.setId(resultSet.getInt(columnIndex"id"));
+				buyItemDTO.setItemName(resultSet.getString("item_name"));
+				buyItemDTO.setItemPrice(resultSet.getString("item_price"));
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return buyItemDTO;
+	}
+	public BuyItemDTO getBuyItemDTO(){
+		return buyItemDTO;
+	}
+
+}
